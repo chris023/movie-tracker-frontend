9b40b919da398cd4b534de72b0fc018707585321
chris023 <chris.meyer023@gmail.com>
2019-05-09 12:11:23 -0600

    Load poster image from tmdb

--- src/components/movie/Poster.js
+++ src/components/movie/Poster.js
@@ -1,16 +1,43 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { withStyles } from '@material-ui/core'
+import { Card, withStyles } from '@material-ui/core'
 
-const styles = () => ({})
+const styles = () => ({
+  root: {
+    height: '100%',
+    width: '100%',
+    position: 'relative',
+  },
+  poster: {
+    width: '100%',
+    height: '100%',
+    zIndex: '-10',
+  },
+  content: {
+    width: '100%',
+    height: '100%',
+    position: 'absolute',
+    top: 0,
+  },
+})
 
-// eslint-disable-next-line no-unused-vars
-const Poster = ({ classes }) => {
-  return <p>Poster</p>
+const Poster = ({ classes, movie }) => {
+  return (
+    <Card className={classes.root}>
+      <img
+        className={classes.poster}
+        src={'http://image.tmdb.org/t/p/w400' + movie.poster_path}
+      />
+      <div className={classes.content}>
+        <p>hi</p>
+      </div>
+    </Card>
+  )
 }
 
 Poster.propTypes = {
   classes: PropTypes.object,
+  movie: PropTypes.object,
 }
 
 export default withStyles(styles)(Poster)
