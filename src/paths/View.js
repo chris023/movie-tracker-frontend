6f67550403d751a9844fb2648b16f1ff2623942f
chris023 <chris.meyer023@gmail.com>
2019-05-09 12:22:19 -0600

    Create logout menu

--- src/paths/View.js
+++ src/paths/View.js
@@ -1,6 +1,17 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
-import { Grid, Paper, withStyles } from '@material-ui/core'
+import {
+  AppBar,
+  Grid,
+  IconButton,
+  Menu,
+  MenuItem,
+  Paper,
+  Toolbar,
+  Typography,
+  withStyles,
+} from '@material-ui/core'
+import PersonIcon from '@material-ui/icons/Person'
 import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import { Poster } from '../components/movie'
@@ -9,23 +20,63 @@ const styles = theme => ({
   root: {
     padding: theme.spacing.unit * 4,
   },
+  appBarSpacer: theme.mixins.toolbar,
+  pushRight: {
+    marginLeft: 'auto',
+  },
 })
 
 const View = ({ classes, fetchMovies, movies }) => {
+  const [anchor, setAnchor] = useState(null)
+  const [open, setOpen] = useState(false)
+  const openMenu = ({ target }) => {
+    setOpen(true)
+    setAnchor(target)
+  }
+  const closeMenu = () => {
+    setOpen(false)
+    setAnchor(null)
+  }
+
   useEffect(() => {
     fetchMovies()
   }, [fetchMovies])
 
   return (
-    <Paper square className={classes.root}>
-      <Grid container spacing={16}>
-        {movies.results.map(movie => (
-          <Grid item xs={3} key={movie.id}>
-            <Poster movie={movie} />
-          </Grid>
-        ))}
-      </Grid>
-    </Paper>
+    <>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h4">Movie Tracker</Typography>
+          <div className={classes.pushRight}>
+            <IconButton onClick={openMenu}>
+              <PersonIcon />
+            </IconButton>
+            <Menu
+              anchorEl={anchor}
+              open={open}
+              onClose={closeMenu}
+              PaperProps={{
+                style: {
+                  width: 200,
+                },
+              }}
+            >
+              <MenuItem>Log Out</MenuItem>
+            </Menu>
+          </div>
+        </Toolbar>
+      </AppBar>
+      <Paper square className={classes.root}>
+        <div className={classes.appBarSpacer} />
+        <Grid container spacing={16}>
+          {movies.results.map(movie => (
+            <Grid item xs={3} key={movie.id}>
+              <Poster movie={movie} />
+            </Grid>
+          ))}
+        </Grid>
+      </Paper>
+    </>
   )
 }
 
