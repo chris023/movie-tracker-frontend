3051f5ab73f3b529302adc0a4edabc8ab6dc4a73
chris023 <chris.meyer023@gmail.com>
2019-05-09 09:45:59 -0600

    Add redux-sagas and boilerplate

--- src/util/redux/store.js
+++ src/util/redux/store.js
@@ -1,8 +1,10 @@
 import { compose } from 'redux'
-import { createStore } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
+import createSagaMiddleware from 'redux-saga'
 import { persistStore, persistReducer } from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 import rootReducer from './reducers'
+import mySaga from './sagas'
 
 const persistConfig = {
   key: 'root',
@@ -11,7 +13,10 @@ const persistConfig = {
 
 const pReducer = persistReducer(persistConfig, rootReducer)
 
+const sagaMiddleware = createSagaMiddleware()
+
 const allStoreEnhancers = compose(
+  applyMiddleware(sagaMiddleware),
   (window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()) ||
     compose
@@ -19,5 +24,7 @@ const allStoreEnhancers = compose(
 
 const store = createStore(pReducer, allStoreEnhancers)
 const persistor = persistStore(store)
+
+sagaMiddleware.run(mySaga)
 
 export { store, persistor }
