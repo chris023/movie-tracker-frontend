9141ae0d4c7200fd5e8d8d6d94c717556fc560d8
chris023 <chris.meyer023@gmail.com>
2019-05-09 15:12:56 -0600

    Dispatch favorite toggle to saga

--- src/components/movie/Poster.js
+++ src/components/movie/Poster.js
@@ -3,12 +3,16 @@ import PropTypes from 'prop-types'
 import { Card, IconButton, withStyles } from '@material-ui/core'
 import FavoriteBorderIcon from '@material-ui/icons/FavoriteBorder'
 import FavoriteIcon from '@material-ui/icons/Favorite'
+import { compose } from 'recompose'
+import { connect } from 'react-redux'
+import { addFav, removeFav } from '../../util/redux/actions'
 
 const styles = () => ({
   root: {
     height: '100%',
     width: '100%',
     position: 'relative',
+    cursor: 'pointer',
   },
   poster: {
     width: '100%',
@@ -23,15 +27,37 @@ const styles = () => ({
   },
 })
 
-const Poster = ({ classes, movie }) => {
+const Poster = ({ classes, movie, addFavorite, removeFavorite, user_id }) => {
   const [isFavorite, setFavorite] = useState(false)
-  const toggleFavorite = () => setFavorite(prev => !prev)
+  const toggleFavorite = () => {
+    switch (isFavorite) {
+      case true:
+        removeFavorite({
+          movie_id: movie.id,
+          user_id,
+          title: movie.title,
+          poster_path: movie.poster_path,
+          release_date: movie.release_date,
+          vote_average: movie.vote_average,
+          overview: movie.overview,
+        })
+        setFavorite(prev => !prev)
+        break
+      case false:
+        addFavorite({})
+        setFavorite(prev => !prev)
+        break
+      default:
+        return
+    }
+  }
 
   return (
     <Card className={classes.root}>
       <img
         className={classes.poster}
         src={'http://image.tmdb.org/t/p/w400' + movie.poster_path}
+        alt={movie.title}
       />
       <div className={classes.content}>
         <IconButton onClick={toggleFavorite}>
@@ -45,6 +71,24 @@ const Poster = ({ classes, movie }) => {
 Poster.propTypes = {
   classes: PropTypes.object,
   movie: PropTypes.object,
+  addFavorite: PropTypes.func,
+  removeFavorite: PropTypes.func,
+  user_id: PropTypes.number,
 }
 
-export default withStyles(styles)(Poster)
+const mapStateToProps = state => ({
+  user_id: state.login.data.id,
+})
+
+const mapDispatchToProps = dispatch => ({
+  addFavorite: movie => dispatch(addFav(movie)),
+  removeFavorite: movie => dispatch(removeFav(movie)),
+})
+
+export default compose(
+  withStyles(styles),
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )
+)(Poster)
