e49761e42bcaea124c2ae3b4f2c8f529db72fc57
chris023 <chris.meyer023@gmail.com>
2019-05-16 11:23:12 -0600

    Remove history on BrowserRouter

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import { Provider } from 'react-redux'
 import { MuiThemeProvider } from '@material-ui/core'
 
 import theme from './util/material-ui/theme'
-import history from './util/router/history'
 import { store } from './util/redux'
 import App from './App'
 
@@ -13,7 +12,7 @@ import './index.css'
 
 ReactDOM.render(
   <Provider store={store}>
-    <BrowserRouter history={history} basename="/movie-tracker-frontend">
+    <BrowserRouter basename="/movie-tracker-frontend">
       <MuiThemeProvider theme={theme}>
         <App />
       </MuiThemeProvider>
